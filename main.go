3d2fde49712b993c84d87c7a6e850dc7110fa233
陈显鹏 <chen5604@qq.com>
2020-11-06 11:24:28 +0800

    save

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/ethereum/go-ethereum/accounts/keystore"
-	"j2pay-server/cron"
 	"j2pay-server/ethclient"
 	"j2pay-server/model"
 	"j2pay-server/pkg/logger"
@@ -25,7 +24,7 @@ func main() {
 	//初始化以太坊节点
 	ethclient.InitClient(fmt.Sprintf("%s", setting.EthConf.Url))
 	//初始化定时器检测
- 	cron.Cron()
+ //	cron.Cron()
 	//网关
 	router := routers.InitRouter()
 	//启动
